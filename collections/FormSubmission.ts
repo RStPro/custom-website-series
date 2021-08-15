89ccacdebf7dce8b8b8d1f9bef184973b820e23e
Rodolfo <rsantos.professional@gmail.com>
2021-08-15 19:54:30 +0100

    add studies
    also passed slug creation to a separate componente

--- collections/FormSubmission.ts
+++ collections/FormSubmission.ts
@@ -8,11 +8,26 @@ const FormSubmission: CollectionConfig = {
       type: 'text',
       name: 'from',
       label: 'From Email',
+      admin: {
+        readOnly: true,
+      },
     },
     {
       type: 'textarea',
       name: 'message',
       label: 'Message',
+      admin: {
+        readOnly: true,
+      },
+    },
+    {
+      type: 'text',
+      name: 'source',
+      label: 'Source',
+      admin: {
+        position: 'sidebar',
+        readOnly: true,
+      },
     },
   ],
 };
